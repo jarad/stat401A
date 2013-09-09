57dd1d9d0eaf1023343b3bec219301e82226774d
Jarad Niemi <niemi@iastate.edu>
2013-09-09 08:15:42 -0500

    added pvalue scripts

---
+++ lab/pvalues.R
@@ -0,0 +1,16 @@
+# For calculative pvalues
+p1 = 1-pnorm(2)         # P(Z > 2)
+p1
+p2 = 1-pt(3,5)          # P(t_5 > 3)
+p2
+p3 = 1-pt(3,5)+pt(-3,5) # P(t_5 > |3|)
+p3
+p4 = 1-pf(5,25,26)      # P(F_25,26 > 5)
+p4
+
+# For constructing confidence intervals
+q1 = qnorm(.975)        # 0.975 = P(Z<q1)
+q1
+q2 = qt(.975,5)         # 0.975 = P(t_5 > q2)
+q2
+
